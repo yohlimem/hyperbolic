175ec4794b75736860bd1ce7b5d86a5fe3c754c9
Yahli Mariansky <yahlimem@gmail.com>
2024-02-06 13:26:14 +0200

    Added fancy colors

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::vec;
 
 use lines::Line;
-use nannou::{ease::circ, geom::point, math::ConvertAngle, prelude::*};
-use nannou_egui::{self, egui::{self, lerp}, Egui};
+use nannou::{draw::mesh::vertex::Color, ease::circ, geom::point, math::ConvertAngle, prelude::*};
+use nannou_egui::{self, color_picker::Alpha, egui::{self, lerp, Color32}, Egui};
 
 mod points;
 mod lines;
@@ -17,6 +17,17 @@ struct Model {
     circles: Vec<(f32, Point, Line)>,
     lines: Vec<Line>,
     node_size: f32,
+
+
+    // circle_color: Rgba8,
+    // line_color: Rgba8,
+    // node_color: Rgba8,
+    // background_color: Rgba8,
+
+    // circle_color_c32: Color32,
+    // line_color_c32: Color32,
+    // node_color_c32: Color32,
+    // background_color_c32: Color32,
 }
 
 fn main() {
@@ -37,30 +48,54 @@ fn model(app: &App) -> Model {
         mouse_down: true,
         lines: vec![],
         node_size: 5.0,
+
+
+        // circle_color: BLANCHEDALMOND.into(),
+        // line_color: Rgba8::new(100, 100, 100, 255),
+        // node_color: Rgba8::new(255, 255, 255, 0),
+        // background_color: Rgba8::new(14, 48, 87, 255),
+
+        // color 32circle_color
+        // circle_color_c32: Color32::from_rgba_premultiplied(255, 255, 255, 255),
+        // line_color_c32: Color32::from_rgba_premultiplied(100, 100, 100, 255),
+        // node_color_c32: Color32::from_rgba_premultiplied(255, 255, 255, 255),
+        // background_color_c32: Color32::from_rgba_premultiplied(14, 48, 87, 255),
     }
 }
 
 fn update(app: &App, model: &mut Model, update: Update) {
+    
+    
     {let egui = &mut model.egui;
-    egui.set_elapsed_time(update.since_start);
-
-    let ctx = egui.begin_frame();
-
-    egui::Window::new("Rum windowSome").show(&ctx, |ui| {
-        ui.label("radius");
-        ui.add(egui::Slider::new(&mut model.circle_radius, 50.0..=2000.0));
-        ui.add(egui::Slider::new(&mut model.node_size, 0.1..=10.0));
-
+        egui.set_elapsed_time(update.since_start);
+        
+        let ctx = egui.begin_frame();
+        
+        egui::Window::new("Rum windowSome").show(&ctx, |ui| {
+            ui.label("radius");
+            ui.add(egui::Slider::new(&mut model.circle_radius, 50.0..=2000.0));
+            ui.add(egui::Slider::new(&mut model.node_size, 0.1..=10.0));
+            
+            // egui::widgets::color_picker::color_edit_button_srgba(ui, &mut model.line_color_c32, Alpha::BlendOrAdditive);
+            // egui::widgets::color_picker::color_edit_button_srgba(ui, &mut model.node_color_c32, Alpha::BlendOrAdditive);
+            // egui::widgets::color_picker::color_edit_button_srgba(ui, &mut model.background_color_c32, Alpha::BlendOrAdditive);
+            // egui::widgets::color_picker::color_edit_button_srgba(ui, &mut model.circle_color_c32, Alpha::BlendOrAdditive);
+            
     });}
-
-    // println!(Some"{}Some", model.num);
-    // model.lines[0].start.pos = app.mouse.position();
-
-    // model.points.clear();
-    draw_lines(&app, &app.draw(), model, 255);
+    // model.circle_color = convert_nannou_color_to_egui_color_mut(model.circle_color_c32);
+    // model.line_color = convert_nannou_color_to_egui_color_mut(model.line_color_c32);
+    // model.node_color = convert_nannou_color_to_egui_color_mut(model.node_color_c32);
+    // model.background_color = convert_nannou_color_to_egui_color_mut(model.background_color_c32);
+        
+        // println!(Some"{}Some", model.num);
+        // model.lines[0].start.pos = app.mouse.position();
+        
+        // model.points.clear();
+        draw_lines(&app, &app.draw(), model, 255);
     // middle of circle = 0.0, 0.0
     // TODO: https://math.stackexchange.com/a/1322475
     // https://www.youtube.com/watch?v=xHCEDMQDbzk
+    // https://www.ms.uky.edu/~droyster/courses/spring08/math6118/classnotes/chapter09.pdf PAGE 4 CASE 3
     model.circles.clear();
     for l in 0..model.lines.len() {
         let p1 = model.lines[l].start;
@@ -96,13 +131,9 @@ fn raw_window_event(_app: &App, model: &mut Model, event: &nannou::winit::event:
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-    draw.background().color(BLACK);
+    draw.background().color(Rgb8::new	(13,9,40));
 
-    draw.ellipse().radius(model.circle_radius);
-    // for p in &model.points {
-    //     p.draw(&draw, None);
-    //     // p.to_hyperbolic_point().draw(&draw);
-    // }
+    draw.ellipse().radius(model.circle_radius).color(Rgb8::new(45,39,102)).stroke_color(Rgb8::new(86,82,171)).stroke_weight(1.0);
 
     
     for line in &model.lines {
@@ -113,7 +144,7 @@ fn view(app: &App, model: &Model, frame: Frame) {
         p2.draw(&draw, model.node_size, None);
     }
     for circle in &model.circles {
-        // draw.ellipse().radius(circle.0).xy(circle.1.pos).color(Rgba8::new(255, 255, 255, 0)).stroke_weight(1.0).stroke_color(BLACK);
+        // draw.ellipse().radius(circle.0).xy(circle.1.pos).color(Rgb8::newa8::new(255, 255, 255, 0)).stroke_weight(1.0).stroke_color(BLACK);
         let segments = generate_segment(circle.0, circle.1, circle.2.start, circle.2.end);
         // circle.2.start.draw(&draw, Some("a"));
         // circle.2.end.draw(&draw, Some("b"));
@@ -122,7 +153,7 @@ fn view(app: &App, model: &Model, frame: Frame) {
         // draw.line().end(vec2(-1000.0, 0.0)).start(vec2(1000.0, 0.0));
         // println!("{:?}", segments);
 
-        draw.polyline().points(segments).color(Rgba8::new(100, 100, 100, 255));
+        draw.polyline().points(segments).color(Rgb8::new(97,189,172));
     }
     {
     // test
@@ -260,6 +291,9 @@ pub fn generate_segment(radius: f32, center: Point, point_a: Point, point_b: Poi
 
 }
 
+fn convert_nannou_color_to_egui_color_mut(color: Color32) -> nannou::color::Rgba8 {
+    nannou::color::Rgba8::new(color.r(), color.g(), color.b(), color.a())
+}
 
 
 
